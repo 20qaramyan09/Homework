389ea2ce2732559da0d35e46e1a5319882c628f2
20qaramyan09 <121562462+20qaramyan09@users.noreply.github.com>
2025-02-11 11:29:50 +0400

    Update '*' նիշերով եռանկյուն տպելու ծրագիր.cpp

--- '*' նիշերով եռանկյուն տպելու ծրագիր.cpp
+++ '*' նիշերով եռանկյուն տպելու ծրագիր.cpp
@@ -5,17 +5,12 @@ int main() {
     std::cout << "Enter the height of the pyramid: ";
     std::cin >> height;
 
-    for (int i = 1; i <= height; ++i) {
-        // Print leading spaces
+    for (int i = 1; i <= height; ++i) 
         for (int j = 0; j < height - i; ++j) {
             std::cout << " ";
-        }
-        // Print stars
         for (int k = 0; k < 2 * i - 1; ++k) {
             std::cout << "*";
         }
-        // Move to the next line
-        std::cout << std::endl;
     }
 
     return 0;
