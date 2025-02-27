47c195dde0de9ac3a6bad19525084ee7110c8c59
20qaramyan09 <121562462+20qaramyan09@users.noreply.github.com>
2025-02-27 15:48:49 +0400

    Update gumar.cpp

--- 03.02.2025/gumar.cpp
+++ 03.02.2025/gumar.cpp
@@ -4,9 +4,11 @@ int main() {
     int number = 0;
     int sum = 0;
 
-    std::cout << "mutqagreq tvanshany ";
-    std::cin >> number;
+    
+    std::cout << "mutqagreq ";
 
+    
+    std::cin >> number
     sum = (number / 1000) + ((number / 100) % 10) + ((number / 10) % 10) + (number % 10);
 
     std::cout << "ariunq" << sum <<std::endl;
